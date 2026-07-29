d93d33713abb0a014fde0f28862005f17398194b
Vad333 <48320151+Vad333@users.noreply.github.com>
2026-07-29 14:30:17 +1000

    Update script2026-07-27.sql

--- script2026-07-27.sql
+++ script2026-07-27.sql
@@ -1,8 +1,1 @@
--- MY_HIVE
-insert into "MY_HIVE" ("HIV_ID", "HIV_NUMB", "HIV_TYPE", "HIV_NAME", "HIV_INFO", "HIV_POS", "HIV_COLOR", "HIV_STATUS", "HIV_ADD") values 
-(10, '3', 4, 'Третий', 'Пустой', 'Дача', 'Синий', 2, NULL);
-
--- MY_INSPECT
-insert into "MY_INSPECT" ("INS_ID", "INS_DATE", "INS_HIVE_ID", "INS_PCHUL", "INS_RAMALL", "INS_RAMRAS", "INS_RAMVOS", "INS_VESMED", "INS_RESULT") values 
-(5, '2026-07-26', 2, NULL, 4, 1, 1, 0, 'Добавлена рамка вощины и 100 мл сиропа'), 
-(6, '2026-07-26', 1, NULL, 8, 3, 2, 0, 'Добавлено 100 мл сиропа');
+insert into "MY_EVENT" ("EVN_ID", "EVN_DATE", "EVN_INFO", "EVN_HIVE_ID") values (1, '2026-07-26', 'Вечером был возможный напад на улей', '2');
